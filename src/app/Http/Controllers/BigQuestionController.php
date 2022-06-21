ce448645c6e1d0cd534fa876cad96438bae9484f
shuma-fukuba <calculus.fukuba@gmail.com>
2022-06-22 08:09:13 +0900

    create models and controllers

--- src/app/Http/Controllers/BigQuestionController.php
+++ src/app/Http/Controllers/BigQuestionController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\BigQuestion;
 use Illuminate\Http\Request;
 
 class BigQuestionController extends Controller
 {
     //
+    public function index(Request $request)
+    {
+        $items = BigQuestion::all();
+        return view('bigQuestion', ['items' => $items]);
+    }
 }
