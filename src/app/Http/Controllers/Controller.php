6f2c53c3ce203a3a1cfaccc51220595807056ffd
shuma-fukuba <calculus.fukuba@gmail.com>
2022-07-10 22:42:14 +0900

    hello world

--- src/app/Http/Controllers/Controller.php
+++ src/app/Http/Controllers/Controller.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function index(Request $request) {
+        $user = Auth::user();
+        $sort = $request->sort();
+    }
 }
