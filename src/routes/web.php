24954b4353cad6f0d6116f3be5923df82beae79f
fukuba-shuma <calculus.fukuba@gmail.com>
2022-08-11 09:31:20 +0900

    regulate login attempts

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,11 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/quiz', 'BigQuestionController@index');
+Route::get('/', 'BigQuestionController@index');
 
 Route::get('/quiz/{id}', 'SmallQuestionController@index')->name('quiz.index');
 
