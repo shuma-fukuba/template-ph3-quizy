b81a8a553f831d533841233618775da6d8f80e18
fukuba-shuma <calculus.fukuba@gmail.com>
2022-09-25 22:13:27 +0900

    rename adminControllers

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Middleware\TraceLog;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,26 +23,26 @@ Auth::routes();
 
 
 Route::prefix('admin')->group(function () {
-    Route::get('/', 'QuestionController@index')->name('admin');
+    Route::get('/', 'adminController@index')->name('admin');
 
-    Route::get('big_questions', 'QuestionController@get_big_questions')->name('admin.big_questions');
+    Route::get('big_questions', 'adminController@get_big_questions')->name('admin.big_questions');
 
-    Route::post('big_questions/create', 'QuestionController@add_big_questions')->name('admin.big_question_add');
+    Route::post('big_questions/create', 'AdminController@add_big_questions')->name('admin.big_question_add');
 
-    Route::get('/admin/big_questions/add', 'QuestionController@big_question_add_form')->name('admin.big_question_add_form');
+    Route::get('/admin/big_questions/add', 'AdminController@big_question_add_form')->name('admin.big_question_add_form');
 
-    Route::get('big_questions/{id}', 'QuestionController@big_question')->name('admin.big_question');
+    Route::get('big_questions/{id}', 'AdminController@big_question')->name('admin.big_question');
 
-    Route::get('big_questions/{id}/update', 'QuestionController@update_big_question_index')->name('admin.big_question.update.index');
+    Route::get('big_questions/{id}/update', 'AdminController@update_big_question_index')->name('admin.big_question.update.index');
 
-    Route::post('big_questions/{id}/update', 'QuestionController@update_big_question')->name('admin.big_question.update');
+    Route::post('big_questions/{id}/update', 'AdminController@update_big_question')->name('admin.big_question.update');
 
-    Route::get('/big_questions/{id}/delete', 'QuestionController@delete_big_quesiton_index')->name('admin.big_question.delete.index');
+    Route::get('/big_questions/{id}/delete', 'AdminController@delete_big_quesiton_index')->name('admin.big_question.delete.index');
 
-    Route::post('big_questions/{id}/delete', 'QuestionController@delete_big_question')->name('admin.big_question.delete');
+    Route::post('big_questions/{id}/delete', 'AdminController@delete_big_question')->name('admin.big_question.delete');
 
     Route::post(
         '/big_questions/order',
-        'QuestionController@update_big_questions_order'
+        'AdminController@update_big_questions_order'
     )->name('admin.update_big_questions_order');
 });
